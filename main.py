cdf698eff07a3482827fec64a0720a71cabb859b
Nicholas Morrison Jemio <nicholasmorrisonjemio@gmail.com>
2019-02-16 09:26:49 +1300

    Adding word count page, post response

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, flash, request
+from flask_wtf import Form
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+import random
+import re
 
 app = Flask(__name__)
+app.secret_key = str(random.randint(1,1000000000000000))
 
 class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-
+    words = TextAreaField('Words')
+    submit = SubmitField("Get Count")
+    results = StringField("Count")
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -16,20 +21,22 @@ def about():
 
 @app.route("/count", methods=['GET', 'POST'])
 def count():
-    form = ReusableForm(request.form)
+    form = ReusableForm()
 
-    print(form.errors)
     if request.method == 'POST':
-        name=request.form['name']
-    print(name)
+        if form.validate() == False:
+            flash('All fields are required!')
+            return render_template('count.html', form=form)
+        else:
+            words = form.words.data
+            print(words)
+            x = re.split("\s", words)
+            form.results.data = len(x)
+            form.process()
+            return render_template('results.html', form=form)
+    elif request.method == 'GET':
+        return render_template('count.html', form=form)
 
-    if form.validate():
-    # Save the comment here.
-        flash('Hello ' + name)
-    else:
-        flash('All the form fields are required. ')
-
-    return render_template('hello.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
